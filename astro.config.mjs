99b1542dbfb677591d5701f72c5dcb436c328d26
jorenbroekema <joren.broekema@gmail.com>
2022-04-01 12:24:37 +0200

    feat: add prism theme, make links open in new tab

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,15 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   buildOptions: {
     site: "https://backlight.dev",
   },
+  markdownOptions: {
+    render: [
+      "@astrojs/markdown-remark",
+      {
+        rehypePlugins: [
+          ["rehype-external-links", { target: "_blank", rel: ["noopener"] }],
+        ],
+        syntaxHighlight: "prism",
+      },
+    ],
+  },
 });
